2cb3e53642223e4fc6033fb858c72b6c4477b3b0
sofiaycamil <estudiantessoficamil@gmail.com>
2025-05-06 13:18:14 -0300

    Add files via upload

---
+++ empresa.sql
@@ -0,0 +1,3 @@
+--INSERT INTO clientes (nombre,edad,dni,direccion,telefono) VALUES ('sofia','15','49507256','calle cordoba','3413150303');
+ --INSERT INTO clientes (nombre,edad,dni,direccion,telefono) VALUES ('sabrina','20','23456719','calle oroño','341453678');
+
